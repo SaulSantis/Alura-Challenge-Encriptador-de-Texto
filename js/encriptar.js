87abd0ddd96504f69bd0ebba1045244f1d362b35
Saúl Santibáñez <saulssantis@gmail.com>
2022-03-11 14:22:52 -0300

    Retoques en css, cambios de clases en html

--- js/encriptar.js
+++ js/encriptar.js
@@ -1,25 +1,27 @@
-ingresoTexto = document.querySelector(".input-encriptar");
+ingresoTexto = document.querySelector(".caja-encriptar");
 botonEncriptar = document.querySelector(".btn-Encriptar");
-mensajeEncriptado = document.querySelector(".input-mensaje");
+mensajeEncriptado = document.querySelector(".caja-mensaje");
 
-function validar(frase) {
+function soloMinusculas(texto) {
   var buscar = /^[a-z ]+$/g;
-  frase = ingresoTexto.value;
-  if (frase.match(buscar)) {
+  texto = ingresoTexto.value;
+  if (texto.match(buscar)) {
   } else {
-    alert("Ufff!! lo siento, solo se permiten letras minúsculas, sin números ni caracteres especiales. Inténtalo otra vez!!");
+    alert(
+      "Ufff!! lo siento, solo se permiten letras minúsculas, sin números ni caracteres especiales. Inténtalo otra vez!!"
+    );
     inputTexto.value = "";
   }
 }
 
 botonEncriptar.addEventListener("click", function (event) {
   event.preventDefault();
-  validar();
+  soloMinusculas();
   texto = ingresoTexto.value;
   textoEncriptado = texto
-  .replace(/e/gi, "enter")
-  .replace(/i/gi, "imes")
-  .replace(/a/gi, "ai")
+    .replace(/e/gi, "enter")
+    .replace(/i/gi, "imes")
+    .replace(/a/gi, "ai")
     .replace(/o/gi, "ober")
     .replace(/u/gi, "ufat");
   mensajeEncriptado.value = textoEncriptado;
